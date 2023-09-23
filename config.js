2d293bc1a21a96461d1cc9adb2f303c3a4e63b4b
nsneha676 <87087491+nsneha676@users.noreply.github.com>
2023-09-23 17:29:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,14 @@
 module.exports = {
   development: {
     database: {
-      host: 'localhost',
-      user: 'root',
-      password: 'root',
-      database: 'ResultManagement'
+      host: 'database-01.cbz57jnlwyud.ap-south-1.rds.amazonaws.com',
+      user: 'admin',
+      password: 'Admin131997',
+      database: 'database-01'
+      Port:3306
     },
     app: {
-      port: 3000,
+      port: 80,
       secret: 'your-secret-key'
     }
   },
